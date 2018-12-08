cbd7d436aded7f92f25e50eddc520b900a1e61d2
cineron <cineron+gh@gmail.com>
2018-12-07 19:42:36 -0600

    Adds beginning Lesson 12

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 console.log("app.js is running!");
 
+//create app object
+var appObj = {
+    title: "Indecision_App",
+    subtitle: "Text from object property"
+}
+
+
 //JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+<div>
+    <h1>{appObj.title}</h1>
+    <p>{appObj.subtitle}</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item one</li>
+    </ol>
+</div>);
+
+//create your own template
+// div, h1:name, p:age, p:location
+var user = {
+    name: "Mike",
+    age: 46,
+    location: "Dallas"
+};
+
+
+var templateTwo = (
+    <div>
+        <h1>{user.name + "!"}!</h1>
+        <p>{user.age}</p>
+        <p>{user.location}</p>
+    </div>
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
