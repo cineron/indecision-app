a942c1ee0216abd5eee85c9ef2c7a0a48164cc5a
cineron <cineron+gh@gmail.com>
2018-12-08 00:15:59 -0600

    uses if statement and JSX to return a value if there is one

--- src/app.js
+++ src/app.js
@@ -23,18 +23,23 @@ var template = (
 var user = {
     name: "Mike",
     age: 46,
-    location: "Dallas"
+    location: ""
 };
 
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    } 
+}
 
 var templateTwo = (
     <div>
         <h1>{user.name + "!"}!</h1>
-        <p>{user.age}</p>
-        <p>{user.location}</p>
+        <p>Age: {user.age}</p>
+        {getLocation(user.location)}
     </div>
 );
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
 // ReactDOM.render(templateTwo, appRoot);
