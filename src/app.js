059fb7b1c36bd210443ccb4e4176a2b3de177595
cineron <cineron+gh@gmail.com>
2018-12-17 23:12:56 -0600

    Beginning of Lesson 18-Forms

--- src/app.js
+++ src/app.js
@@ -5,49 +5,17 @@ const appObj = {
     subtitle: "Let your computer decide.",
     options: ["one", "two"]
 };
-
 const template = (
-<div>
-    <h1>{appObj.title}</h1>
-    {appObj.subtitle && <h2>{appObj.subtitle}</h2>}
-    <p>{appObj.options.length > 0 ? "Your options" : "No options"}</p>
-    <ol>
-        <li>item one</li>
-        <li>item two</li>
-    </ol>
-</div>
+    <div>
+        <h1>{appObj.title}</h1>
+        {appObj.subtitle && <h2>{appObj.subtitle}</h2>}
+        <p>{appObj.options.length > 0 ? "Your options" : "No options"}</p>
+        <ol>
+            <li>item one</li>
+            <li>item two</li>
+        </ol>
+    </div>
 );
 
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterapp();
-};
-const minusOne = () => {
-    count--;
-    renderCounterapp();
-    console.log("minusONe");
-};
-const reset = () => {
-    count = 0;
-    renderCounterapp();
-    console.log("reset");
-};
-
 const appRoot = document.getElementById("app");
-
-const renderCounterapp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-
-}
-
-renderCounterapp();
+ReactDOM.render(template, appRoot);
