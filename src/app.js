2e531dfc0876db9f651c10d6290255cdafa79a7e
cineron <cineron+gh@gmail.com>
2019-01-05 21:23:02 -0600

    Adds babel-core and babel-loader to Webpack. Through lesson 55

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const template = React.createElement('p',{},"testing 1,2,3...");
+const template = <p>testing 1,2,3...<br/> using JSX!!!</p>;
 
 ReactDOM.render(template, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,14 @@ module.exports = {
     output: {
         path: path.join(__dirname, "public"),
         filename: "bundle.js"
+    },
+    // add loader, which is in a module
+    // see webpack.js.org
+    module: {
+        rules: [{
+            loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 };
